6c5f657b70635e4d506c4af7d132d9b89df9e2d4
jdavis3333 <jdavis3333@gmail.com>
2020-06-22 22:56:37 -0700

    created questions and answers in array

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 var highScores = document.getElementById("View High Scores")
 var timeEl = document.getElementById("Timer")
-var submit = document.getElementById("submit")
+var getStarted = document.getElementById("start")
+
+
+
+
+//Quiz Questions
+
+var myQuestions = [
+    new question("Who invented JavaScript?", ["Douglas Crockford", "Sheryl Sandberg", "Brenden Eich", "Jerome Chenette"], "Brenden Eich"),
+    new question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
+    new question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
+    new question("Webdevtrick.com is about..", ["Web Design", "Graphic Design", "SEO & Development", "All"], "All"),
+    new question("Who invented JQuery?", ["John Resig", "Timmy Willison", "Bill Gates", "Jeff Bezos"], "John Resig"),
+
+
+   
+]
+
+
