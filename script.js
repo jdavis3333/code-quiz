3e41011075cd0e02e88f1b4a75098b9a3a77aea4
jdavis3333 <jdavis3333@gmail.com>
2020-06-23 20:04:00 -0700

    added two divs to index.html and created corresponding pointers in JS

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+var highScores = document.getElementById("ViewHighScores")
 var highScoresEl = document.getElementById("highScoresContainer")
 var timeEl = document.querySelector(".time")
 var getStarted = document.getElementById("start")
 var mainEl  = document.getElementById("container")
 var questionsEl = document.getElementById("questionsContainer")
+var newQuestion = document.getElementById("newQuestion")
+var choice1 = document.getElementById("choice1")
+var choice2 = document.getElementById("choice2")
+var choice3 = document.getElementById("choice3")
+var choice4 = document.getElementById("choice4")
+var highScoreInitials = document.getElementById("initials")
+var scoreSubmit = document.getElementById("scoreSubmit")
+
 
 
 var secondsLeft = 60;
@@ -34,7 +43,7 @@ for (var i = 0; i < myQuestions.length; i++) {
     }
 }
 
-getStarted.addEventListener('click', StartGame())
+getStarted.addEventListener('click', startGame())
 
 //Create timer countdown
 function setTime() {
@@ -51,22 +60,23 @@ function setTime() {
     }, 1000)
 }
 
-// //start game
+//start game
 function startGame() {
     setTime();
     askQuestions();
 }
 
-function askQuestions() {
-    mainEl.textContent = "";
-    var newQuestion = document.createElement("h3");
-    newQuestion.innerHTML(myQuestions[i]);
-    mainEl.appendChild(newQuestion)
-    var choice1 = document.createElement("button");
-    choice1.setAttribute("button", "")
-}
+// function askQuestions() {
+//     mainEl.textContent = "";
+//     var newQuestion = document.createElement("h3");
+//     newQuestion.innerHTML(myQuestions[i]);
+//     mainEl.appendChild(newQuestion)
+//     var choice1 = document.createElement("button");
+//     choice1.setAttribute("button", "")
+// }
 
 function highScorePage() {
     window.textContent = "";
     document.getElementById("result").innerHTML = localStorage.getItem()
 }
+
